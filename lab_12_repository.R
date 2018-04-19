e91f1fb46635ee8312ff9bf368d158587f1dfd3a
lindayang3932 <34386337+lindayang3932@users.noreply.github.com>
2018-04-19 19:02:01 -0400

    task 2a}

---
+++ lab_12_repository.R
@@ -0,0 +1,11 @@
+generate_data = function(n, p){
+  covariates = matrix(1,n,p)
+  responses = vector("integer")
+  for (i in 1:np){
+    covariates[i] = rnorm(1)
+  }
+  for (i in 1:n){
+    responses[i] = rnorm(1)
+  }
+  return(c(covariates = covariates, responses = responses))
+}
